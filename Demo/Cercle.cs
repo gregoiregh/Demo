fca0d858beb7cc54a69a76d05b8e35e8a6ad7b4d
Ghislaine GREGOIRE <gg@2isa.com>
2018-11-07 22:14:58 +0100

    depot initial

---
+++ Demo/Cercle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Cercle
+    {
+        public int Rayon { get; set; }
+        public Cercle(int rayon)
+        {
+            this.Rayon = rayon;
+        }
+        public override string ToString()
+        {
+            return $"Rayon = {Rayon}";
+        }
+    }
+}
